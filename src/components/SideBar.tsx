efa4f2223ef6a970628edd640d75e75a322c0eea
Wesley <wrodrigo33@gmail.com>
2022-07-09 15:17:19 -0300

    feat: usando react-virtualized sidebar

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,5 +1,7 @@
 import { Button } from "./Button";
 
+import { List, ListRowRenderer } from 'react-virtualized';
+
 interface SideBarProps {
   genres: Array<{
     id: number;
@@ -16,20 +18,33 @@ export function SideBar({
   buttonClickCallback
 }: SideBarProps) {
 
+  const rowRenderer: ListRowRenderer = (({ index, key, style }) => {
+    return (
+      <div key={key} style={style}>
+        <Button
+          key={String(genres[index].id)}
+          title={genres[index].title}
+          iconName={genres[index].name}
+          onClick={() => buttonClickCallback(genres[index].id)}
+          selected={selectedGenreId === genres[index].id}
+        />
+      </div>
+    )
+  });
+
   return (
     <nav className="sidebar">
       <span>Watch<p>Me</p></span>
 
       <div className="buttons-container">
-        {genres.map(genre => (
-          <Button
-            key={String(genre.id)}
-            title={genre.title}
-            iconName={genre.name}
-            onClick={() => buttonClickCallback(genre.id)}
-            selected={selectedGenreId === genre.id}
-          />
-        ))}
+        <List
+          width={320}
+          height={500}
+          rowHeight={80}
+          overscanRowCount={5}
+          rowCount={genres.length}
+          rowRenderer={rowRenderer}
+        />
       </div>
     </nav>
   )
